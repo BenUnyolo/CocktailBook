6dbf89de5b935efd14a9ae9f8c68bf658cbb268b
Ben Unyolo <benunyolo@gmail.com>
2020-10-16 18:12:55 -0400

    adding build folder

---
+++ client/build/precache-manifest.dc2a13eb68a53af6d5a3fd146254b5c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69e686126cb737e4e79268f66a7a1ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f5d8a2d91e616023e44",
+    "url": "/static/css/main.503cc10b.chunk.css"
+  },
+  {
+    "revision": "5b52fded31afb8e8301f",
+    "url": "/static/js/2.6badb064.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6badb064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5d8a2d91e616023e44",
+    "url": "/static/js/main.b4cf0ac9.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "d55ddbf2d7e23f02149fae5e431c8764",
+    "url": "/static/media/bartender.d55ddbf2.svg"
+  },
+  {
+    "revision": "da64d8c624e418ce92504ba617cb4268",
+    "url": "/static/media/cocktail.da64d8c6.svg"
+  },
+  {
+    "revision": "93ec9d1310afa7043db6eec2d2e29ab9",
+    "url": "/static/media/explosion.93ec9d13.svg"
+  }
+]);
